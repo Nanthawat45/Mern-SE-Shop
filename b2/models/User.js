12a4fa32c064cec6d7f4dbc31ce457153e0904fc
Nanthawat45 <Nanthawat.ohm45@gmail.com>
2025-04-11 19:57:29 +0700

    The only thing left is to order

--- b2/models/User.js
+++ b2/models/User.js
@@ -3,7 +3,7 @@ const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
-  role: { type: String, enum: ["admin", "user"], default: "admin" },
+  role: { type: String, enum: ["admin", "user"], default: "user" },
 });
 
 const UserModel = model("User", UserSchema);
--- backend/routers/stripe.router.js
+++
@@ -1,8 +0,0 @@
-const exportss   = require('express');
-const router = exportss.Router();
-const stripeController = require('../controllers/stripes.controller');
-
-router.post('/create-checkout-session', stripeController.createCheckOutSession);
-router.post('/webhook', stripeController.webhook);
-
-module.exports = router;
